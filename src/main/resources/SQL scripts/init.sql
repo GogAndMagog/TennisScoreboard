af178b11c927547adb853601ec41e9d570d3e4b5
GogAndMagog <sergejsmel6@gmail.com>
2024-11-07 17:17:18 +0300

    build(Resources): add config files and sql-scripts

---
+++ src/main/resources/SQL scripts/init.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS Players, Matches;
+DROP INDEX IF EXISTS players_name;
+
+CREATE TABLE Players
+(
+    ID   INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    Name VARCHAR(30)
+);
+CREATE UNIQUE INDEX players_name ON Players (Name);
+
+CREATE TABLE Matches
+(
+    ID      INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    Player1 INTEGER NOT NULL,
+    Player2 INTEGER NOT NULL,
+    Winner  INTEGER NOT NULL,
+    UUID    UUID    NOT NULL,
+    FOREIGN KEY (Player1) REFERENCES Players (ID),
+    FOREIGN KEY (Player2) REFERENCES Players (ID),
+    FOREIGN KEY (Winner) REFERENCES Players (ID)
+);
+
+CREATE INDEX matches_player1 ON Matches (Player1);
+CREATE INDEX matches_player2 ON Matches (Player2);
+
